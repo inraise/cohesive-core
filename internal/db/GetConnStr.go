cdd8d2b0a6d35cefb90957cabcb9313587c0cf10
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-06-19 10:29:09 +0300

    feat(db): подключение к БД, запуск миграций

---
+++ internal/db/GetConnStr.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"fmt"
+	"os"
+)
+
+func GetConnStr() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+}
