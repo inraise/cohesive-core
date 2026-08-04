ab5a642e84e6c5d6ae8b5cd5543b0e0874edda38
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-08-04 06:24:00 +0300

    fix: размер email в ограничениях varchar

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
     id            UUID         PRIMARY KEY     DEFAULT uuid_generate_v4(),
     version       INT                 NOT NULL DEFAULT 1,
-    email         VARCHAR(255) UNIQUE NOT NULL CHECK(char_length(email) BETWEEN 5 AND 100),
+    email         VARCHAR(100) UNIQUE NOT NULL CHECK(char_length(email) BETWEEN 5 AND 100),
     password_hash VARCHAR(255)        NOT NULL,
     first_name    VARCHAR(100)        NOT NULL CHECK(char_length(first_name) BETWEEN 1 AND 100),
     last_name     VARCHAR(100)                 CHECK(char_length(last_name) BETWEEN 1 AND 100),
