a5be4fae747ea86f32ca00f7b93f7632eb023c13
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-08-17 23:54:19 +0300

    feat: сервисный слой создания семьи

--- internal/features/households/service/service.go
+++ internal/features/households/service/service.go
@@ -1,16 +1,28 @@
 package households_service
 
-type HouseHoldsService struct {
-	houseHoldsRepository HouseHoldsRepository
+import (
+	core_domain "cohesive-core/internal/core/domain"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type HouseholdsService struct {
+	householdsRepository HouseholdsRepository
 }
 
-type HouseHoldsRepository interface {
+type HouseholdsRepository interface {
+	CreateHousehold(
+		ctx context.Context,
+		household core_domain.Household,
+		ownerID uuid.UUID,
+	) (core_domain.Household, error)
 }
 
-func NewHouseHoldsService(
-	houseHoldsRepository HouseHoldsRepository,
-) *HouseHoldsService {
-	return &HouseHoldsService{
-		houseHoldsRepository: houseHoldsRepository,
+func NewHouseholdsService(
+	householdsRepository HouseholdsRepository,
+) *HouseholdsService {
+	return &HouseholdsService{
+		householdsRepository: householdsRepository,
 	}
 }
