915ddcef64c6b539bc0ac000ad800ddf3af7895a
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-09-04 14:09:09 +0300

    feat: транспортный слой tasks

--- internal/features/tasks/transport/http/transport.go
+++ internal/features/tasks/transport/http/transport.go
@@ -1,1 +1,14 @@
 package tasks_transport_http
+
+import core_transport_http_server "cohesive-core/internal/core/transport/http/server"
+
+type TasksHTTPHandler struct {
+	tasksService TaskService
+}
+
+type TaskService interface {
+}
+
+func (h *TasksHTTPHandler) Routes() []core_transport_http_server.Route {
+	return []core_transport_http_server.Route{}
+}
