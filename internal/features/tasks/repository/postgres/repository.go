94a9afafd54c81bc019d5c2099de193d37bd2719
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-09-04 14:13:35 +0300

    feat: слой репозитория repository

--- internal/features/tasks/repository/postgres/repository.go
+++ internal/features/tasks/repository/postgres/repository.go
@@ -1,1 +1,15 @@
 package tasks_repository_postgres
+
+import core_pool "cohesive-core/internal/core/repository/postgres/pool"
+
+type TasksRepository struct {
+	pool core_pool.Pool
+}
+
+func NewAuthRepository(
+	pool core_pool.Pool,
+) *TasksRepository {
+	return &TasksRepository{
+		pool: pool,
+	}
+}
