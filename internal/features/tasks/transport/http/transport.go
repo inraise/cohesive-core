336f7562600835d10946cb10cb3676aec0bd2804
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-09-05 18:24:44 +0300

    feat: транспортный слой для задачи, эндпоинт для создания задачи

--- internal/features/tasks/transport/http/transport.go
+++ internal/features/tasks/transport/http/transport.go
@@ -1,22 +1,51 @@
 package tasks_transport_http
 
-import core_transport_http_server "cohesive-core/internal/core/transport/http/server"
+import (
+	core_domain "cohesive-core/internal/core/domain"
+	core_jwt "cohesive-core/internal/core/jwt"
+	core_transport_http_middleware "cohesive-core/internal/core/transport/http/middleware"
+	core_transport_http_server "cohesive-core/internal/core/transport/http/server"
+	tasks_service "cohesive-core/internal/features/tasks/service"
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 type TasksHTTPHandler struct {
-	tasksService TaskService
+	tasksService TasksService
+	tokenManager *core_jwt.TokenManager
 }
 
-type TaskService interface {
+type TasksService interface {
+	CreateTask(
+		ctx context.Context,
+		householdID uuid.UUID,
+		callerID uuid.UUID,
+		request tasks_service.CreateTaskRequest,
+	) (core_domain.Task, error)
 }
 
 func NewTasksHTTPHandler(
-	tasksService TaskService,
+	tasksService TasksService,
+	tokenManager *core_jwt.TokenManager,
 ) *TasksHTTPHandler {
 	return &TasksHTTPHandler{
 		tasksService: tasksService,
+		tokenManager: tokenManager,
 	}
 }
 
 func (h *TasksHTTPHandler) Routes() []core_transport_http_server.Route {
-	return []core_transport_http_server.Route{}
+	authenticate := core_transport_http_middleware.Authenticate(h.tokenManager)
+	withAuth := []core_transport_http_middleware.Middleware{authenticate}
+
+	return []core_transport_http_server.Route{
+		{
+			Method:     http.MethodPost,
+			Path:       "/households/{id}/tasks",
+			Handler:    h.CreateTask,
+			Middleware: withAuth,
+		},
+	}
 }
