a9298c4936cdd300d14af0890ce2c1949793cd6d
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-09-14 22:49:15 +0300

    feat: создание новой транзакции

---
+++ internal/features/budget/repository/postgres/create_transaction.go
@@ -0,0 +1,32 @@
+package budget_repository_postgres
+
+import (
+	core_domain "cohesive-core/internal/core/domain"
+	"context"
+)
+
+func (r *BudgetRepository) CreateTransaction(
+	ctx context.Context,
+	tx core_domain.HouseholdTransaction,
+) (core_domain.HouseholdTransaction, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+		INSERT INTO household_transactions (household_id, type, amount, description, created_by, created_at, updated_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		RETURNING id, household_id, version, type, amount, description, created_by, created_at, updated_at;
+	`
+
+	row := r.pool.QueryRow(ctx, query,
+		tx.HouseholdID,
+		string(tx.Type),
+		tx.Amount,
+		tx.Description,
+		tx.CreatedBy,
+		tx.CreatedAt,
+		tx.UpdatedAt,
+	)
+
+	return scanTransaction(row)
+}
